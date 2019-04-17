e54de26f413b698d0d7a09a0a29677709f7a3b42
Choudhary, Yogesh <Yogesh.Choudhary@capita.co.uk>
2019-04-17 05:41:50 +0530

    Renamed solution and project

--- FunctionOne/BooksLib.cs
+++ FunctionOne/BooksLib.cs
@@ -1,13 +1,10 @@
-
-using System.IO;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Host;
-using Newtonsoft.Json;
 
-namespace FunctionOne
+namespace BookLib
 {
     public static class BooksLib
     {
